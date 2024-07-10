137dd1724382c4b84e777ca1c0eb94aaa4fd310a
AntonioNeto504 <antoniocustorioneto@gmail.com>
2024-07-10 14:39:05 -0300

    feat: Implementing db connection with repository

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -4,5 +4,4 @@ trips_routes_bp = Blueprint("trip_routes", __name__)
 
 @trips_routes_bp.route("/trips", methods=["POST"])
 def create_trip():
-    return jsonify({"ola": "mundo"}), 200
-
+    return jsonify({ "ola": "mundo" }), 200
